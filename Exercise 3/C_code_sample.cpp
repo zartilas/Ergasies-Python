59e8139bda02340e5344ac2fd169691ac6d44271
Vasilios <zartilasvasilios@hotmail.gr>
2019-10-06 16:27:52 +0000

    Added C_code_sample.cpp

---
+++ Exercise 3/C_code_sample.cpp
@@ -0,0 +1,13 @@
+/* This is a comment */
+
+/* C++ comments can also
+   * span multiple lines
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello, World!";
+    return 0;
+}
