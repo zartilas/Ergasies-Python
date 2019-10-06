081ef924e14c59a3bae94f6ef73d9da1030bc1ba
Vasilios <zartilasvasilios@hotmail.gr>
2019-10-06 20:31:35 +0000

    Updated Main.py

--- Exercise 2/Main.py
+++ Exercise 2/Main.py
@@ -1,3 +1,21 @@
 from FileManager import *
-from Counter import *
+
+file_exists('Plithos.txt')
+
+# Non printable characters are (ascii table): 0-31 & 127, therefore 32 + 1 = 33 non printable characters
+file = open('Plithos.txt', 'w+')
+file.write("Number of all non printable characters: " + str(33) + "\n"
+           + "\n"
+           + "ASCII table of non-printable characters:\n"
+           + "Dec  Char\n"
+           + "---------\n")
+non_ascii = ["\b", "\t", " ", "\u007F"]
+for i in range(0, 4):
+    file.write(str(ord(non_ascii[i])) + "  " + non_ascii[i]
+               .replace(" ", "SPACE")
+               .replace("\u007F", "DELETE")
+               .replace("\b", "BACKSPACE")
+               .replace("\t", "TAB")
+               + "\n")
+file.close()
 
