ed849b7ad6159f716a3540e3a98239ca04d86d93
Vasilios <zartilasvasilios@hotmail.gr>
2019-10-06 16:23:43 +0000

    Added main.py

---
+++ Exercise 1/Main.py
@@ -0,0 +1,9 @@
+from FindPrimes import *
+
+print("Prime Numbers between range of 1-1000:")
+print(primes)
+
+max_diff = primes[len(primes) - 1] - primes[0]
+print()
+print("Maximum difference between range of 1-1000:")
+print(max_diff)
